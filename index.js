5118b53289d5f873cf3e77fd2cf4df35c2d3bbfa
Dmitry Marov <d.marov94@gmail.com>
2018-03-20 16:40:43 +0300

    get auth token added

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const Koa = require('koa');
+const Router = require('koa-better-router');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const json = require('koa-json');
+
+let app = new Koa();
+let route = Router().loadMethods();
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3987;
+
+route.get('/auth-token', ctx => {
+
+    try {
+
+        let secret = fs.readFileSync('keys/private.key');
+        let token = jwt.sign({ permissions: ['write'] }, secret, { algorithm: 'RS512', expiresIn: '24h' });
+        ctx.body = { status: 'ok', token: token };
+
+    } catch (e) {
+
+        ctx.body = { status: 'err', message: e.message };
+
+    }
+
+});
+
+route.get('/refresh-token', ctx => {
+
+    // ctx.body = 'refresh token sent';
+
+});
+
+let apiRoute = Router({ prefix: '/api/v1' });
+apiRoute.extend(route);
+
+app.use(json());
+app.use(apiRoute.middleware());
+app.listen(PORT);
