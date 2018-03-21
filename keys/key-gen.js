ed747321130c586a723659de4a6ad9b6015be05c
Dmitry Marov <d.marov94@gmail.com>
2018-03-21 13:51:26 +0300

    keys generation script added

---
+++ keys/key-gen.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const minimist = require('minimist');
+const exec = require('child_process').exec;
+
+const argv = minimist(process.argv.slice(2));
+
+let privateKeyPath = argv['priv'] ? argv['priv'] : 'private.key';
+let publicKeyPath = argv['pub'] ? argv['pub'] : 'public.key';
+
+exec(`openssl genrsa -out ${privateKeyPath} 2048`, _ => {
+
+    exec(`openssl rsa -in ${privateKeyPath} -pubout -out ${publicKeyPath}`); 
+
+});
