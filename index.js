c5d1bdb68b47815b0dec5e69eeebca584105938e
Dmitry Marov <d.marov94@gmail.com>
2018-04-13 12:14:25 +0300

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const config = require('./lib/Config');
 const Koa = require('koa');
+const jsonBody = require('koa-json-body');
 const Router = require('koa-better-router');
 const fs = require('fs');
 const path = require('path');
@@ -12,16 +13,21 @@ const jwt = require('jsonwebtoken');
 let app = new Koa();
 let route = Router().loadMethods();
 
+let secret = fs.readFileSync(path.resolve(config.privateKeyPath));
+let params = { algorithm: 'RS512', expiresIn: config.tokenExpiresIn };
+
+app.use(jsonBody({fallback: true}));
+
+// should get user name from server
+
+
 route.get('/authorization-token', async ctx => {
 
     try {
 
-        let data = { permissions: ['write'] };
-        let secret = fs.readFileSync(path.resolve(config.privateKeyPath));
-        let params = { algorithm: 'RS512', expiresIn: config.tokenExpiresIn };
-
+        let data = ctx.request.body;
         let token = jwt.sign(data, secret, params);
-        ctx.body = { 'authorization-token': token };
+        ctx.response.body = { 'authorization-token': token };
 
     } catch (e) {
 
